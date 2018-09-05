a6a6821adff0dfdb6d3f4681edb6c7b5f4de3140
villi <vilhjalmur12@ru.is>
2018-09-05 17:55:49 +0000

    listening server & random range client

--- client_triple.cpp
+++ client_triple.cpp
@@ -8,6 +8,7 @@
 #include <netdb.h>
 
 #include <map>
+#include <unistd.h>
 
 void error(const char *msg)
 {
@@ -17,9 +18,25 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
+    /*
+      Initizialise variables
+    */
     int sockfd, portno, n;
     struct sockaddr_in serv_addr, socket_addr;           // Socket address structure
     struct hostent *server;
+    int begin_port = 1025;
+    int end_port = 60000;
+    unsigned int sleeptime = 500000;
+
+    // bool hash map for which ports are finished
+    std::map<int, bool> port_map;
+    int ports_counted = end_port - begin_port;
+    //typedef std::map<int, bool>::iterator map_iter;
+
+    if (argc > 2) {
+      begin_port = atoi(argv[2]);
+      end_port = atoi(argv[3]);
+    }
 
     char buffer[256];
     if (argc < 3) {
@@ -27,62 +44,57 @@ int main(int argc, char *argv[])
        exit(0);
     }
 
-    portno = atoi(argv[2]);     // Read Port No from command line
+    portno = end_port;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); // Open Socket
+    while ((end_port - begin_port) > (port_map.size())) {
 
-
-    if (sockfd < 0)
-        error("ERROR opening socket");
-
-    server = gethostbyname(argv[1]);        // Get host from IP
-
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET; // This is always set to AF_INET
-
-    // Host address is stored in network byte order
-    bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-
-    // bool hash map for which ports are finished
-    std::map<int, bool> port_map;
-    int ports_counted;
-    typedef std::map<int, bool>::iterator map_iter;
-    int begin_port = 2500;
-    int end_port = 2600;
-    portno = begin_port;
-
-    for (int iter = begin_port; iter <= end_port; iter++) {
-      port_map[iter] = false;
-    }
-
-    while (ports_counted <= 100) {
-      serv_addr.sin_port = htons(portno);
-
-      if(port_map[portno]) {
+      if(port_map[portno] == true) {
+        portno = begin_port + (rand() % static_cast<int>(end_port - begin_port + 1));
         continue;
       }
+      //loop_test_count = 0;
+      sockfd = socket(AF_INET, SOCK_STREAM, 0); // Open Socket
+
+      if (sockfd < 0)
+          error("ERROR opening socket");
+
+      server = gethostbyname(argv[1]);        // Get host from IP
+
+      if (server == NULL) {
+          fprintf(stderr,"ERROR, no such host\n");
+          exit(0);
+      }
+
+      bzero((char *) &serv_addr, sizeof(serv_addr));
+
+      serv_addr.sin_family = AF_INET; // This is always set to AF_INET
+
+      // Host address is stored in network byte order
+      bcopy((char *)server->h_addr,
+           (char *)&serv_addr.sin_addr.s_addr,
+           server->h_length);
+
+
+      serv_addr.sin_port = htons(portno);
 
       if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
-          printf("Port %d NOT open\n", portno);
+          //printf("Port %d NOT open\n", portno);
       }
       else {
           printf("Port %d IS open\n", portno);
       }
 
       port_map[portno] = true;
-      ports_counted++;
-      portno++;
+      ports_counted--;
+      portno = begin_port + (rand() % static_cast<int>(end_port - begin_port + 1));
+
+      // Set sleep on
+      //usleep(sleeptime);
+
+      close(sockfd);
     }
 
-    /*
+    /*  If we want to read and write to socket
 
     serv_addr.sin_port = htons(portno);
 
@@ -100,7 +112,6 @@ int main(int argc, char *argv[])
     if (n < 0)
          error("ERROR writing to socket");
 
-
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
     if (n < 0)
@@ -108,6 +119,10 @@ int main(int argc, char *argv[])
     printf("%s\n",buffer);
     */
 
-    close(sockfd);
+    // TODO: Final tests
+    //printf("port map size: %lu\n", port_map.size());
+    //printf("end - begin: %d\n", (end_port - begin_port));
+    //printf(port_map.at(2500) ? "2500: True\n" : "2500: False\n");
+
     return 0;
 }
