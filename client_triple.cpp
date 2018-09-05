36998f277d3c65e040faeec2d9150cb6c241c939
villi <vilhjalmur12@ru.is>
2018-09-05 10:20:03 +0000

    loopa fyrir triple handshake með map, virkar ekki að opna samt

--- client_triple.cpp
+++ client_triple.cpp
@@ -5,7 +5,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
+
+#include <map>
 
 void error(const char *msg)
 {
@@ -16,7 +18,7 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
-    struct sockaddr_in serv_addr;           // Socket address structure
+    struct sockaddr_in serv_addr, socket_addr;           // Socket address structure
     struct hostent *server;
 
     char buffer[256];
@@ -29,7 +31,8 @@ int main(int argc, char *argv[])
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); // Open Socket
 
-    if (sockfd < 0) 
+
+    if (sockfd < 0)
         error("ERROR opening socket");
 
     server = gethostbyname(argv[1]);        // Get host from IP
@@ -44,14 +47,49 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET; // This is always set to AF_INET
 
     // Host address is stored in network byte order
-    bcopy((char *)server->h_addr, 
+    bcopy((char *)server->h_addr,
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
 
+    // bool hash map for which ports are finished
+    std::map<int, bool> port_map;
+    int ports_counted;
+    typedef std::map<int, bool>::iterator map_iter;
+    int begin_port = 2500;
+    int end_port = 2600;
+    portno = begin_port;
+
+    for (int iter = begin_port; iter <= end_port; iter++) {
+      port_map[iter] = false;
+    }
+
+    while (ports_counted <= 100) {
+      serv_addr.sin_port = htons(portno);
+
+      if(port_map[portno]) {
+        continue;
+      }
+
+      if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+          printf("Port %d NOT open\n", portno);
+      }
+      else {
+          printf("Port %d IS open\n", portno);
+      }
+
+      port_map[portno] = true;
+      ports_counted++;
+      portno++;
+    }
+
+    /*
+
     serv_addr.sin_port = htons(portno);
 
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
+
+
 
     // Read and write to socket
     printf("Please enter the message: ");
@@ -59,14 +97,17 @@ int main(int argc, char *argv[])
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
 
-    if (n < 0) 
+    if (n < 0)
          error("ERROR writing to socket");
+
 
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
-    if (n < 0) 
+    if (n < 0)
          error("ERROR reading from socket");
     printf("%s\n",buffer);
+    */
+
     close(sockfd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -27,26 +27,29 @@ int main(int argc, char *argv[])
 
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
 
      bzero((char *) &serv_addr, sizeof(serv_addr));
+     // portno check
+     printf("The portno is: %d\n", portno);
+
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
 
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+              sizeof(serv_addr)) < 0)
               error("ERROR on binding");
 
      // Listen on socket for traffic
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
+     newsockfd = accept(sockfd,
+                 (struct sockaddr *) &cli_addr,
                  &clilen);
-     if (newsockfd < 0) 
+     if (newsockfd < 0)
           error("ERROR on accept");
      bzero(buffer,256);
      n = read(newsockfd,buffer,255);
@@ -60,5 +63,5 @@ int main(int argc, char *argv[])
 
      close(newsockfd);
      close(sockfd);
-     return 0; 
+     return 0;
 }
